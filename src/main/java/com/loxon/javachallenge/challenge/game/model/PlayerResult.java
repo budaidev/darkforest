299b638082de13d6d226c3ec4e2d920e0fcc6246
Csaba Forjan <iimperator1555@gmail.com>
2023-05-13 19:11:50 +0200

    update api

---
+++ src/main/java/com/loxon/javachallenge/challenge/game/model/PlayerResult.java
@@ -0,0 +1,12 @@
+package com.loxon.javachallenge.challenge.game.model;
+
+import lombok.Data;
+
+@Data
+public class PlayerResult {
+	private int playerId;
+	private int score;
+	private int numOfColinizedPlanets;
+	private int numOfDestroyedHostilePlanets;
+	private int numOfDeflectedMBH;
+}
